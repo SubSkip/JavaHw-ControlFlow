a47dc62dfbaaca9b1376c4d71531490a167ca8da
SubSkip <craig.e.zieg@gmail.com>
2020-12-14 12:53:30 -0600

    commit

---
+++ java_flow_control.java
@@ -0,0 +1,22 @@
+
+/* ********************
+Instructions included in Control Flow - PROJECT (4) (1).md
+********************** */
+import java.util.Scanner;
+
+public class java_flow_control {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        /*
+         * public static void printNumbers() { // TODO: print valid numeric input }
+         * 
+         * public static void printLowerCase() { // TODO: print valid lowercase
+         * alphabetic input }
+         * 
+         * public static void printUpperCase() { // TODO: print valid uppercase
+         * alphabetic input }
+         */
+        userInput.close();
+    }
+}
